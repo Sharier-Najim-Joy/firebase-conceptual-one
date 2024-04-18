7a545144c266fa1b29218e954aca75e405cdf12f
Sharier-Najim-Joy <shariernajim.joy.1@gmail.com>
2024-04-18 18:44:56 +0600

    second commit

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,8 +1,12 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
 
 const Register = () => {
+    const navigate = useNavigate();
+    const [error,setError] =useState('');
+
+
     const {registerUser} = useContext(AuthContext);
     const handleRegister = e =>{
         e.preventDefault();
@@ -12,18 +16,37 @@ const Register = () => {
         const password = e.target.password.value;
         const confirm_password = e.target.confirm_password.value;
         console.log(name,photo,email,password,confirm_password);
-        
+
+        if(password.length<6){
+            setError('password must be 6 characters')
+            return; 
+        }
+        if(password !== confirm_password){
+            setError('password does not match')
+            return;
+        }
+        if(!/^(?=.*\d)(?=.*[^\w\s])(?=.*[a-zA-Z]).*$/.test(password)){
+            setError('password must be number,symbol and word')
+            return;
+        }
+
+        setError('')
+
         // firebase register
         registerUser(email,password)
         .then(result=>{
             console.log(result.user);
             e.target.reset();
+            navigate('/')
         })
         .catch(error=>{
-            console.log(error);
+            console.log(error.message);
+            setError('email-already-in-use')
         })
 
     }
+
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col">
@@ -49,12 +72,14 @@ const Register = () => {
                                 <span className="label-text">Email</span>
                             </label>
                             <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                           
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
                             <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                            
                         </div>
                         <div className="form-control">
                             <label className="label">
@@ -65,6 +90,9 @@ const Register = () => {
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
                         </div>
+                        {
+                            error && <small className="text-red-700">{error}</small>
+                        }
                         <div className="form-control mt-6">
                             <button className="btn btn-primary">Register</button>
                         </div>
